54111341c888882ac0dd1393f4b5d54273bebe75
Mark Rodriguez <Markrodriguez003@gmail.com>
2024-09-10 22:53:17 -0400

    Fixed minor google captcha problem

--- src/components/ui/ContactForm/index.jsx
+++ src/components/ui/ContactForm/index.jsx
@@ -102,7 +102,7 @@ export default function ContactForm() {
     // console.log(JSON.stringify(formData));
     setEmailSuccess(await contactUsEmail(formData));
     const captchaValue = recaptcha.current.getValue();
-    console.log('captucha value: ', captchaValue);
+    console.log('captucha value : ', captchaValue);
     if (!captchaValue) {
       SAlert.fire({
         confirmButtonColor: "black",
